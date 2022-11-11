46eea424326ec91b6804fdda46b8249610752a4c
Steven Uray <steven.uray@gmail.com>
2022-11-11 13:39:17 -0800

    Starting to build out public subnets

--- terraform/aws/modules/networking/main.tf
+++ terraform/aws/modules/networking/main.tf
@@ -1,8 +1,8 @@
 locals {
   cidr_blocks = {
     "production" = cidrsubnet("10.0.0.0/14", 2, 0)
-    "staging" = cidrsubnet("10.0.0.0/14", 2, 1)
-    "dev" = cidrsubnet("10.0.0.0/14", 2, 2)
+    "staging"    = cidrsubnet("10.0.0.0/14", 2, 1)
+    "dev"        = cidrsubnet("10.0.0.0/14", 2, 2)
   }
 }
 
@@ -10,20 +10,20 @@ resource "aws_vpc" "primary" {
   cidr_block = local.cidr_blocks[var.environment_name]
 
   tags = {
-    Project = "automation-calculator",
+    Project     = "automation-calculator",
     Environment = "dev",
-    SourceRepo = "https://github.com/team-automation-calculator/core-iac"
+    SourceRepo  = "https://github.com/team-automation-calculator/core-iac"
   }
 }
 
-resource "aws_subnet" "main_1a" {
-  vpc_id = aws_vpc.primary.id
-  cidr_block = cidrsubnet(aws_vpc.primary.cidr_block, 2, 0)
+resource "aws_subnet" "public_1a" {
+  vpc_id            = aws_vpc.primary.id
+  cidr_block        = cidrsubnet(aws_vpc.primary.cidr_block, 2, 0)
   availability_zone = "${var.availability_zone}a"
 }
 
-resource "aws_subnet" "main_1b" {
-  vpc_id = aws_vpc.primary.id
-  cidr_block = cidrsubnet(aws_vpc.primary.cidr_block, 2, 1)
+resource "aws_subnet" "public_1b" {
+  vpc_id            = aws_vpc.primary.id
+  cidr_block        = cidrsubnet(aws_vpc.primary.cidr_block, 2, 1)
   availability_zone = "${var.availability_zone}b"
 }
--- terraform/aws/modules/networking/outputs.tf
+++ terraform/aws/modules/networking/outputs.tf
@@ -1,9 +1,9 @@
 output "eks_subnet_ids" {
   description = "The subnet ids in the VPC given to EKS"
-  value = [aws_subnet.main_1a.id, aws_subnet.main_1b.id]
+  value       = [aws_subnet.public_1a.id, aws_subnet.public_1b.id]
 }
 
 output "vpc" {
   description = "The vpc created by this module to be used for the given region/env combo"
-  value = aws_vpc.primary
+  value       = aws_vpc.primary
 }
