8a3479ef9ac79e0bd35fbe6f7ebdd1c0fdc24d44
Steven Uray <steven.uray@gmail.com>
2022-11-11 13:37:03 -0800

    Adding vpc output

--- terraform/aws/modules/networking/outputs.tf
+++ terraform/aws/modules/networking/outputs.tf
@@ -1,4 +1,9 @@
 output "eks_subnet_ids" {
-  value = [aws_subnet.main_1a.id, aws_subnet.main_1b.id]
   description = "The subnet ids in the VPC given to EKS"
+  value = [aws_subnet.main_1a.id, aws_subnet.main_1b.id]
+}
+
+output "vpc" {
+  description = "The vpc created by this module to be used for the given region/env combo"
+  value = aws_vpc.primary
 }
