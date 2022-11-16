fb9e1df549ee61ed52cced667fd4300fafba5f69
Steven Uray <steven.uray@gmail.com>
2022-11-15 20:56:35 -0800

    Adding initial rds DB to app module

--- terraform/aws/modules/main_rails_app/variables.tf
+++ terraform/aws/modules/main_rails_app/variables.tf
@@ -1,7 +1,8 @@
 # Declare vars
-variable "project_tag" {
-  type    = string
-  default = "automation-calculator"
+variable "database_instance_class" {
+  type        = string
+  default     = "db.t4g.micro"
+  description = "Amazon RDS instance type/class for the app database in this env"
 }
 
 variable "environment_name" {
@@ -12,4 +13,9 @@ variable "environment_name" {
 variable "eks_subnet_ids" {
   type        = list(any)
   description = "Must be a list of length 2 of aws vpc subnet ids to give the eks cluster"
+}
+
+variable "project_tag" {
+  type    = string
+  default = "automation-calculator"
 }
