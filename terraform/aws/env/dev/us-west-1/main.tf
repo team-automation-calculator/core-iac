a60cecee59201062fbd03e146ebbc310efc0ba24
Steven Uray <steven.uray@gmail.com>
2022-11-18 13:47:11 -0800

    Changing service ipv4 cidr to something valid

--- terraform/aws/env/dev/us-west-1/main.tf
+++ terraform/aws/env/dev/us-west-1/main.tf
@@ -26,7 +26,7 @@ module "networking_layer" {
 }
 
 module "automation_calculator_app_infra" {
-  eks_service_ipv4_cidr = "10.0.2.0/14"
+  eks_service_ipv4_cidr = "10.100.0.0/16"
   eks_subnet_ids        = [module.networking_layer.public_eks_subnet_ids[0], module.networking_layer.private_eks_subnet_ids[0]]
   environment_name      = "dev"
   source                = "../../../modules/main_rails_app"
