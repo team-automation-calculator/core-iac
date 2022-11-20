6e70e67d1c3fd6070b1a436fbfef17444768b424
Steven Uray <stevenuray@users.noreply.github.com>
2022-11-19 16:54:37 -0800

    Changing service_cidr to try different one (#14)

--- terraform/aws/env/dev/us-west-1/main.tf
+++ terraform/aws/env/dev/us-west-1/main.tf
@@ -27,7 +27,7 @@ module "automation_calculator_app_infra" {
 
 module "eks_cluster" {
   environment_name  = var.environment_name
-  service_ipv4_cidr = "10.100.0.0/16"
+  service_ipv4_cidr = "172.20.0.0/16"
   source            = "../../../modules/eks_cluster"
   subnet_ids        = concat(module.networking_layer.public_eks_subnet_ids, module.networking_layer.private_eks_subnet_ids)
   vpc_id            = module.networking_layer.vpc.id
