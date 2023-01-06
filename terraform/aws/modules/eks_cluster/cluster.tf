2ea2bf93471912877e200ec1b59fb1d41c9a898a
Steven Uray <steven.uray@gmail.com>
2023-01-06 12:29:16 -0800

    Removing problematic resources

--- terraform/aws/modules/eks_cluster/cluster.tf
+++ terraform/aws/modules/eks_cluster/cluster.tf
@@ -50,24 +50,6 @@ module "alb_controller_irsa_role" {
   }
 }
 
-resource "kubernetes_service_account" "alb-controller-service-account" {
-  depends_on = [
-    module.app_eks_cluster
-  ]
-  metadata {
-    name      = "aws-load-balancer-controller"
-    namespace = "kube-system"
-    labels = {
-      "app.kubernetes.io/name"      = "aws-load-balancer-controller"
-      "app.kubernetes.io/component" = "controller"
-    }
-    annotations = {
-      "eks.amazonaws.com/role-arn"               = module.alb_controller_irsa_role.iam_role_arn
-      "eks.amazonaws.com/sts-regional-endpoints" = "true"
-    }
-  }
-}
-
 resource "helm_release" "alb-ingress-controller" {
   atomic = true
   chart  = "aws-load-balancer-controller"
@@ -100,20 +82,3 @@ resource "helm_release" "alb-ingress-controller" {
 
   version = "1.4.6"
 }
-
-resource "kubernetes_secret_v1" "alb_ingress_controller_irsa_token" {
-  depends_on = [
-    module.app_eks_cluster
-  ]
-  metadata {
-    annotations = {
-      "kubernetes.io/service-account.name" = "aws-load-balancer-controller"
-    }
-    name      = "aws-load-balancer-controller-token"
-    namespace = "kube-system"
-  }
-
-  type                           = "kubernetes.io/service-account-token"
-  wait_for_service_account_token = true
-}
-
