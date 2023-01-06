6017911ab5782da0eee8f2e0f778b5cec85a14b0
Steven Uray <stevenuray@users.noreply.github.com>
2023-01-06 11:32:06 -0800

    Adding port 9443 to security group to support alb load balancer controller (#48)
    * Adding detail to ingress
    
    * Adding annotations for service
    
    * Adding annotations for ingress in values
    
    * Adding port 9443 to security group to support alb load balancer controller
    
    * Trying source_node_security_group

--- terraform/aws/modules/eks_cluster/cluster.tf
+++ terraform/aws/modules/eks_cluster/cluster.tf
@@ -1,6 +1,24 @@
 module "app_eks_cluster" {
   cluster_version = var.cluster_version
   cluster_name    = "ac_app_${var.environment_name}"
+  cluster_security_group_additional_rules = {
+    alb_webhook_ingress = {
+      description                = "Allow traffic for ALB webhooks on ingress creation"
+      type                       = "ingress"
+      source_node_security_group = true
+      from_port                  = 9443
+      to_port                    = 9443
+      protocol                   = "tcp"
+    },
+    alb_webhook_egress = {
+      description                = "Allow traffic for ALB webhooks on ingress creation"
+      type                       = "egress"
+      source_node_security_group = true
+      from_port                  = 9443
+      to_port                    = 9443
+      protocol                   = "tcp"
+    }
+  }
 
   eks_managed_node_group_defaults = {
     disk_size      = 20
@@ -11,6 +29,7 @@ module "app_eks_cluster" {
     primary = var.node_group_scaling_config
   }
 
+  enable_irsa               = true
   manage_aws_auth_configmap = false
   source                    = "terraform-aws-modules/eks/aws"
   subnet_ids                = var.subnet_ids
