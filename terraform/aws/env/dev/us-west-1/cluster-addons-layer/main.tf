f73dce5875957ea17d084a98b4b1630c9c5e32dc
Steven Uray <steven.uray@gmail.com>
2023-01-06 15:48:53 -0800

    Fixing paths

--- terraform/aws/env/dev/us-west-1/cluster-addons-layer/main.tf
+++ terraform/aws/env/dev/us-west-1/cluster-addons-layer/main.tf
@@ -48,7 +48,7 @@ provider "kubernetes" {
 }
 
 module "automation_calculator_app_infra" {
-  automation_calculator_helm_release_local_path = "../../../../../helm/automation-calculator"
+  automation_calculator_helm_release_local_path = "../../../../../../helm/automation-calculator"
   environment_name                              = var.environment_name
-  source                                        = "../../../modules/main_rails_app"
+  source                                        = "../../../../modules/main_rails_app"
 }
