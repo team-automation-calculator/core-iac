52fc8955a8273d07446c6b0c0de22ab31491c02f
Steven Uray <steven.uray@gmail.com>
2023-01-05 19:25:18 -0800

    initial attempt at adding irsa for alb controller

--- terraform/aws/modules/eks_cluster/cluster.tf
+++ terraform/aws/modules/eks_cluster/cluster.tf
@@ -18,16 +18,30 @@ module "app_eks_cluster" {
   vpc_id                    = var.vpc_id
 }
 
+module "alb_controller_irsa_role" {
+  source = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+
+  role_name                              = "${var.environment_name}_alb_controller_irsa_role"
+  attach_load_balancer_controller_policy = true
+
+  oidc_providers = {
+    main = {
+      provider_arn               = module.app_eks_cluster.oidc_provider_arn
+      namespace_service_accounts = ["kube-system:aws-load-balancer-controller"]
+    }
+  }
+}
+
 resource "kubernetes_service_account" "service-account" {
   metadata {
-    name = "aws-load-balancer-controller"
+    name      = "aws-load-balancer-controller"
     namespace = "kube-system"
     labels = {
-        "app.kubernetes.io/name"= "aws-load-balancer-controller"
-        "app.kubernetes.io/component"= "controller"
+      "app.kubernetes.io/name"      = "aws-load-balancer-controller"
+      "app.kubernetes.io/component" = "controller"
     }
     annotations = {
-      "eks.amazonaws.com/role-arn" = module.lb_role.arn
+      "eks.amazonaws.com/role-arn"               = module.alb_controller_irsa_role.iam_role_arn
       "eks.amazonaws.com/sts-regional-endpoints" = "true"
     }
   }
