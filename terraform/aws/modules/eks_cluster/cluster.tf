dab05e8297998571e3246baaa0a1f26da14f60b2
Steven Uray <stevenuray@users.noreply.github.com>
2023-01-05 19:59:41 -0800

    Initial attempt at adding irsa for alb ingress controller (#42)
    * Initial work on making irsa outside of alb chart
    
    * initial attempt at adding irsa for alb controller
    
    * Filling out additional detail for cluster alb controller object
    
    * Appeasing tf fmt

--- terraform/aws/modules/eks_cluster/cluster.tf
+++ terraform/aws/modules/eks_cluster/cluster.tf
@@ -18,9 +18,41 @@ module "app_eks_cluster" {
   vpc_id                    = var.vpc_id
 }
 
+module "alb_controller_irsa_role" {
+  source = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+
+  role_name                              = "${var.environment_name}_alb_controller_irsa_role"
+  attach_load_balancer_controller_policy = true
+
+  oidc_providers = {
+    main = {
+      provider_arn               = module.app_eks_cluster.oidc_provider_arn
+      namespace_service_accounts = ["kube-system:aws-load-balancer-controller"]
+    }
+  }
+}
+
+resource "kubernetes_service_account" "alb-controller-service-account" {
+  metadata {
+    name      = "aws-load-balancer-controller"
+    namespace = "kube-system"
+    labels = {
+      "app.kubernetes.io/name"      = "aws-load-balancer-controller"
+      "app.kubernetes.io/component" = "controller"
+    }
+    annotations = {
+      "eks.amazonaws.com/role-arn"               = module.alb_controller_irsa_role.iam_role_arn
+      "eks.amazonaws.com/sts-regional-endpoints" = "true"
+    }
+  }
+}
+
 resource "helm_release" "alb-ingress-controller" {
-  atomic     = true
-  chart      = "aws-load-balancer-controller"
+  atomic = true
+  chart  = "aws-load-balancer-controller"
+  depends_on = [
+    kubernetes_service_account.alb-controller-service-account
+  ]
   name       = "aws-load-balancer-controller"
   namespace  = "kube-system"
   repository = "https://aws.github.io/eks-charts"
@@ -28,6 +60,21 @@ resource "helm_release" "alb-ingress-controller" {
   set {
     name  = "clusterName"
     value = module.app_eks_cluster.cluster_name
+  }
+
+  set {
+    name  = "serviceAccount.create"
+    value = "false"
+  }
+
+  set {
+    name  = "serviceAccount.name"
+    value = "aws-load-balancer-controller"
+  }
+
+  set {
+    name  = "vpcId"
+    value = var.vpc_id
   }
 
   version = "1.4.6"
