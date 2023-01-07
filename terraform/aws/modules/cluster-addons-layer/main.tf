5528a9ca40b6be5147792ae961e0d46e6cacc870
Steven Uray <steven.uray@gmail.com>
2023-01-07 10:03:12 -0800

    Try encapsulating provider_arn in quotes

--- terraform/aws/modules/cluster-addons-layer/main.tf
+++ terraform/aws/modules/cluster-addons-layer/main.tf
@@ -6,7 +6,7 @@ module "aws_load_balancer_controller_irsa_role" {
 
   oidc_providers = {
     main = {
-      provider_arn               = var.eks_cluster_oidc_provider_arn
+      provider_arn               = "${var.eks_cluster_oidc_provider_arn}"
       namespace_service_accounts = ["kube-system:aws-load-balancer-controller"]
     }
   }
