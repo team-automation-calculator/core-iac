a57410f645d84fabc8ae29d7cf12ce8502572bf3
Steven Uray <steven.uray@gmail.com>
2023-01-07 09:25:25 -0800

    Re-making aws_load_balancer_controller_irsa_role in cluster-addons-layer, refactoring other layers to accomodate

--- terraform/aws/modules/base-cluster-layer/outputs.tf
+++ terraform/aws/modules/base-cluster-layer/outputs.tf
@@ -9,3 +9,7 @@ output "eks_cluster_api_endpoint" {
 output "eks_cluster_cert_data" {
   value = module.app_eks_cluster.cluster_certificate_authority_data
 }
+
+output "eks_cluster_oidc_provider_arn" {
+  value = module.app_eks_cluster.oidc_provider_arn
+}
