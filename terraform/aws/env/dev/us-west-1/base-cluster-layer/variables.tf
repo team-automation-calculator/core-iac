35fc70f2adf6c4ba162366ccfb25d1ce229ccf96
Steven Uray <steven.uray@gmail.com>
2023-01-09 09:08:40 -0800

    Set empty default, marvel at how this somehow makes it work

--- terraform/aws/env/dev/us-west-1/base-cluster-layer/variables.tf
+++ terraform/aws/env/dev/us-west-1/base-cluster-layer/variables.tf
@@ -13,6 +13,7 @@ variable "environment_name" {
 variable "TF_VAR_GITHUB_TOKEN" {
   description = "Environment variable for the GitHub Personal Access Token to be used by Terraform Cloud to access the GitHub repository"
   type        = string
+  default     = ""
 }
 
 variable "project_tag" {
