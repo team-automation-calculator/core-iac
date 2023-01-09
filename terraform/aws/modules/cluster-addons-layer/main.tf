0f0a00286499678f22fc80586395351268dcb195
Steven Uray <steven.uray@gmail.com>
2023-01-08 18:51:14 -0800

    Adding saved changes

--- terraform/aws/modules/cluster-addons-layer/main.tf
+++ terraform/aws/modules/cluster-addons-layer/main.tf
@@ -89,7 +89,6 @@ resource "helm_release" "external-dns" {
     value = "public"
   }
 
-<<<<<<< HEAD
   set {
     name  = "serviceAccount.create"
     value = "false"
@@ -141,11 +140,3 @@ resource "kubernetes_secret_v1" "external_dns_service_account" {
   type                           = "kubernetes.io/service-account-token"
   wait_for_service_account_token = true
 }
-=======
-  # Using the service account created by this chart
-  set {
-    name = "serviceAccount.create"
-    value = "true"
-  }
-}
->>>>>>> main
