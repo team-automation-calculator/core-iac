ff25f9839f553d0586d998e751226f8bf34f8f0e
Steven Uray <steven.uray@gmail.com>
2023-01-16 12:49:17 -0800

    Try different way of tolist()

--- terraform/aws/env/dev/us-west-1/cluster-addons-layer/main.tf
+++ terraform/aws/env/dev/us-west-1/cluster-addons-layer/main.tf
@@ -70,7 +70,7 @@ module "main_rails_app" {
   automation_calculator_helm_release_local_path = "../../../../../../helm/automation-calculator"
   automation_calculator_app_host                = var.automation_calculator_app_host
   db_security_group_ids                         = data.aws_launch_template.target_cluster_launch_template.vpc_security_group_ids
-  db_subnet_group_ids                           = tolist(data.tfe_outputs.base_layer_state.nonsensitive_values.private_eks_subnet_ids.value)
+  db_subnet_group_ids                           = tolist(data.tfe_outputs.base_layer_state.nonsensitive_values.private_eks_subnet_ids)
   db_port                                       = 5432
   depends_on = [
     module.cluster_addons
