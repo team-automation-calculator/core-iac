59a07fda555cc903d5f2b6e7cd5088fcf1c48974
Steven Uray <steven.uray@gmail.com>
2023-01-16 11:21:58 -0800

    Try nonsensitive_values

--- terraform/aws/env/dev/us-west-1/cluster-addons-layer/main.tf
+++ terraform/aws/env/dev/us-west-1/cluster-addons-layer/main.tf
@@ -70,7 +70,7 @@ module "automation_calculator_app_infra" {
   automation_calculator_helm_release_local_path = "../../../../../../helm/automation-calculator"
   automation_calculator_app_host                = var.automation_calculator_app_host
   db_security_group_ids                         = data.aws_launch_template.target_cluster_launch_template.vpc_security_group_ids
-  db_subnet_group_ids                           = data.tfe_outputs.base_layer_state.values.db_subnet_group_ids
+  db_subnet_group_ids                           = data.tfe_outputs.base_layer_state.nonsensitive_values.db_subnet_group_ids
   db_port                                       = 5432
   depends_on = [
     module.cluster_addons
