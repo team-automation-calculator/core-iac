46e74f8146b2258b6b1b315d5d6f57e64803d87b
Steven Uray <steven.uray@gmail.com>
2023-01-27 12:30:22 -0800

    Updating base cluster layer module

--- terraform/env/development/aws/us-west-1/base-cluster-layer/main.tf
+++ terraform/env/development/aws/us-west-1/base-cluster-layer/main.tf
@@ -4,10 +4,6 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 4.47"
     }
-    tfe = {
-      source  = "hashicorp/tfe"
-      version = "0.41.0"
-    }
   }
 }
 
@@ -24,30 +20,14 @@ provider "aws" {
   }
 }
 
-provider "tfe" {
-  hostname = "app.terraform.io"
-}
-
-resource "tfe_oauth_client" "github" {
-  organization     = var.tf_cloud_organization_name
-  api_url          = "https://api.github.com"
-  http_url         = "https://github.com"
-  service_provider = "github"
-  oauth_token      = var.TF_VAR_GITHUB_TOKEN
-}
-
 module "eks_cluster" {
-  environment_name                       = var.environment_name
-  source                                 = "../../../../modules/base-cluster-layer"
-  subnet_ids                             = module.networking_layer.private_eks_subnet_ids
-  tfe_oauth_client_token_id              = tfe_oauth_client.github.oauth_token_id
-  tf_cloud_organization_name             = var.tf_cloud_organization_name
-  tf_cloud_workspace_path                = "terraform/aws/env/${var.environment_name}/${var.aws_region}/base-cluster-layer"
-  tf_cloud_workspace_vcs_repo_identifier = var.tf_cloud_workspace_vcs_repo_identifier
-  vpc_id                                 = module.networking_layer.vpc.id
+  environment_name = var.environment_name
+  source           = "../../../../../modules/aws/base-cluster-layer"
+  subnet_ids       = module.networking_layer.private_eks_subnet_ids
+  vpc_id           = module.networking_layer.vpc.id
 }
 
 module "networking_layer" {
   environment_name = var.environment_name
-  source           = "../../../../modules/networking"
+  source           = "../../../../../modules/aws/networking"
 }
