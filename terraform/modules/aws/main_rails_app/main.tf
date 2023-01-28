5c2030dc640981762713fccbcc87cfaefc74bee4
Steven Uray <steven.uray@gmail.com>
2023-01-28 08:36:44 -0800

    Fix issue with app host not being passed down correctly to templatefile in main_rails_app module

--- terraform/modules/aws/main_rails_app/main.tf
+++ terraform/modules/aws/main_rails_app/main.tf
@@ -2,6 +2,7 @@ data "template_file" "automation_calculator_helm_chart_values" {
   template = file("${path.module}/values.yml")
   vars = {
     cert_arn = tostring(aws_acm_certificate.automation_calculator_app.arn)
+    automation_calculator_app_host = var.automation_calculator_app_host
   }
 }
 
