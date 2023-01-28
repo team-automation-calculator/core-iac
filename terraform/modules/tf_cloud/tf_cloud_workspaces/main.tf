c13bb1449f7035cd13e9e4ff11edb2830940112d
Steven Uray <steven.uray@gmail.com>
2023-01-28 10:37:02 -0800

    Formatting

--- terraform/modules/tf_cloud/tf_cloud_workspaces/main.tf
+++ terraform/modules/tf_cloud/tf_cloud_workspaces/main.tf
@@ -1,20 +1,20 @@
 resource "tfe_workspace" "base_cluster_tfe_workspace" {
-  name         = "ac_app_base_cluster_layer_${var.environment_name}"
-  organization = var.tf_cloud_organization_name
+  name                      = "ac_app_base_cluster_layer_${var.environment_name}"
+  organization              = var.tf_cloud_organization_name
   remote_state_consumer_ids = [tfe_workspace.cluster_addons_tfe_workspace.id]
   tag_names                 = ["automation-calculator"]
-  trigger_prefixes = var.base_cluster_layer_working_directory
+  trigger_prefixes          = var.base_cluster_layer_working_directory
   vcs_repo {
     identifier     = var.tf_cloud_workspace_vcs_repo_identifier
     oauth_token_id = var.tfe_oauth_client_token_id
   }
-  working_directory         = var.base_cluster_layer_working_directory
+  working_directory = var.base_cluster_layer_working_directory
 }
 
 resource "tfe_workspace" "cluster_addons_tfe_workspace" {
-  name         = "ac_app_cluster_addons_layer_${var.environment_name}"
-  organization = var.tf_cloud_organization_name  
-  tag_names         = ["automation-calculator"]
+  name             = "ac_app_cluster_addons_layer_${var.environment_name}"
+  organization     = var.tf_cloud_organization_name
+  tag_names        = ["automation-calculator"]
   trigger_prefixes = var.cluster_addons_layer_working_directory
   vcs_repo {
     identifier     = var.tf_cloud_workspace_vcs_repo_identifier
