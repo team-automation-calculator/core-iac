07545bf74444e7b6c241f67c9ccb692dd70c547b
Steven Uray <steven.uray@gmail.com>
2023-01-27 19:22:28 -0800

    Initial staging cluster addons layer

---
+++ terraform/env/staging/aws/us-west-1/cluster-addons-layer/variables.tf
@@ -0,0 +1,41 @@
+variable "aws_region" {
+  default     = "us-west-1"
+  description = "AWS Region to deploy the stack to, i.e us-west-1, us-east-1, etc"
+  type        = string
+}
+
+variable "automation_calculator_app_host" {
+  default     = "automation-calculations.io"
+  description = "The host name of the automation-calculator-app"
+  type        = string
+}
+
+variable "project_tag" {
+  default     = "automation_calculator"
+  description = "Tag for describing the name of the project, i.e automation-calculator"
+  type        = string
+}
+
+variable "tf_cloud_organization_name" {
+  default     = "team-automation-calculator"
+  description = "The Terraform Cloud organization name to use for the Terraform Cloud workspace for this layer"
+  type        = string
+}
+
+variable "tfe_cluster_addons_layer_workspace_name" {
+  default     = "core-iac"
+  description = "The name of the Terraform Cloud workspace for the base layer."
+  type        = string
+}
+
+variable "tf_cloud_workspace_path" {
+  default     = "terraform/aws/env/staging/us-west-1/cluster-addons-layer"
+  description = "The path to the Terraform Cloud workspace."
+  type        = string
+}
+
+variable "tfe_base_layer_workspace_name" {
+  default     = "core-iac"
+  description = "The name of the Terraform Cloud workspace for the base layer."
+  type        = string
+}
