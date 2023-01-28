330b03509c56238db43f04e0d7e2aade7f976678
Steven Uray <steven.uray@gmail.com>
2023-01-28 14:18:08 -0800

    Adding variables for module directories to trigger plans/runs on changes to modules

--- terraform/modules/tf_cloud/tf_cloud_workspaces/main.tf
+++ terraform/modules/tf_cloud/tf_cloud_workspaces/main.tf
@@ -3,7 +3,7 @@ resource "tfe_workspace" "base_cluster_tfe_workspace" {
   organization              = var.tf_cloud_organization_name
   remote_state_consumer_ids = [tfe_workspace.cluster_addons_tfe_workspace.id]
   tag_names                 = ["automation-calculator"]
-  trigger_prefixes          = [var.base_cluster_layer_working_directory]
+  trigger_prefixes          = concat([var.base_cluster_layer_working_directory], var.base_cluster_layer_module_directories)
   vcs_repo {
     identifier     = var.tf_cloud_workspace_vcs_repo_identifier
     oauth_token_id = var.tfe_oauth_client_token_id
@@ -15,7 +15,7 @@ resource "tfe_workspace" "cluster_addons_tfe_workspace" {
   name             = "ac_app_cluster_addons_layer_${var.environment_name}"
   organization     = var.tf_cloud_organization_name
   tag_names        = ["automation-calculator"]
-  trigger_prefixes = [var.cluster_addons_layer_working_directory]
+  trigger_prefixes = concat([var.cluster_addons_layer_working_directory], var.cluster_addons_layer_module_directories)
   vcs_repo {
     identifier     = var.tf_cloud_workspace_vcs_repo_identifier
     oauth_token_id = var.tfe_oauth_client_token_id
