0bf763750886384248c242aa92fd4734a647ba1c
Steven Uray <steven.uray@gmail.com>
2023-02-09 17:05:33 -0800

    Bumping app version to disallow /visitor in robots.txt

--- terraform/modules/aws/main_rails_app/variables.tf
+++ terraform/modules/aws/main_rails_app/variables.tf
@@ -12,7 +12,7 @@ variable "app_image_repo" {
 }
 
 variable "app_version" {
-  default     = "0.6.1-584"
+  default     = "0.6.3-586"
   description = "The version of the automation-calculator app to deploy."
   type        = string
 }
