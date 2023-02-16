1cb59ea63bff4df681d69862cb5d29bf1227764a
Steven Uray <steven.uray@gmail.com>
2023-02-16 15:28:15 -0800

    Bump app imae to 0.9.0

--- terraform/modules/aws/main_rails_app/variables.tf
+++ terraform/modules/aws/main_rails_app/variables.tf
@@ -12,7 +12,7 @@ variable "app_image_repo" {
 }
 
 variable "app_version" {
-  default     = "0.8.3-656"
+  default     = "0.9.0-664"
   description = "The version of the automation-calculator app to deploy."
   type        = string
 }
