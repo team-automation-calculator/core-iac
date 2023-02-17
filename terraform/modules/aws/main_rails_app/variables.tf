1d24d8120c4d2905e8a3dd189b7d8046dcbfb29c
Steven Uray <steven.uray@gmail.com>
2023-02-17 15:31:27 -0800

    Bump app version to 0.9.2

--- terraform/modules/aws/main_rails_app/variables.tf
+++ terraform/modules/aws/main_rails_app/variables.tf
@@ -12,7 +12,7 @@ variable "app_image_repo" {
 }
 
 variable "app_version" {
-  default     = "0.9.1-668"
+  default     = "0.9.2-670"
   description = "The version of the automation-calculator app to deploy."
   type        = string
 }
