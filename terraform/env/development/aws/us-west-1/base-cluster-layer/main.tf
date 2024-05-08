492a86e1dacd9aed3f86b5646d5ee2b85111e466
Steven Uray <steven.uray@gmail.com>
2024-05-07 21:38:11 -0700

    Updating dev env kubernetes cluster version

--- terraform/env/development/aws/us-west-1/base-cluster-layer/main.tf
+++ terraform/env/development/aws/us-west-1/base-cluster-layer/main.tf
@@ -32,6 +32,7 @@ module "eks_cluster" {
   environment_name = var.environment_name
   source           = "../../../../../modules/aws/base-cluster-layer"
   subnet_ids       = module.networking_layer.private_eks_subnet_ids
+  cluster_version  = var.kubernetes_cluster_version
   vpc_id           = module.networking_layer.vpc.id
 }
 
