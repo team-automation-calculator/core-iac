fa3fe932bba27cc1b4dff45bb0477adaa493543f
Steven Uray <steven.uray@gmail.com>
2024-05-07 21:38:11 -0700

    Updating dev env kubernetes cluster version

--- terraform/env/development/aws/us-west-1/base-cluster-layer/terraform.tfvars
+++ terraform/env/development/aws/us-west-1/base-cluster-layer/terraform.tfvars
@@ -1,2 +1,3 @@
-environment_name = "development"
-project_tag      = "automation-calculator"
+environment_name           = "development"
+project_tag                = "automation-calculator"
+kubernetes_cluster_version = "1.25"
--- terraform/env/development/aws/us-west-1/base-cluster-layer/variables.tf
+++ terraform/env/development/aws/us-west-1/base-cluster-layer/variables.tf
@@ -9,6 +9,12 @@ variable "environment_name" {
   type        = string
 }
 
+variable "kubernetes_cluster_version" {
+  description = "The kubernetes cluster version"
+  type        = string
+  default     = "1.24"
+}
+
 variable "project_tag" {
   default     = "automation_calculator"
   description = "Tag for describing the name of the project, i.e automation-calculator"
