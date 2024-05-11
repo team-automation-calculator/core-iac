2ab56dd722cdcb4979989f99d3128a171413e0be
Steven Uray <steven.uray@gmail.com>
2024-05-11 13:04:28 -0700

    Formatting

--- terraform/env/staging/aws/us-west-1/base-cluster-layer/main.tf
+++ terraform/env/staging/aws/us-west-1/base-cluster-layer/main.tf
@@ -30,7 +30,7 @@ provider "aws" {
 
 module "eks_cluster" {
   environment_name = var.environment_name
-  cluster_version = var.kubernetes_cluster_version
+  cluster_version  = var.kubernetes_cluster_version
   source           = "../../../../../modules/aws/base-cluster-layer"
   subnet_ids       = module.networking_layer.private_eks_subnet_ids
   vpc_id           = module.networking_layer.vpc.id
